4dd2787df737e9ed16e011e3e04f88be0969cf38
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-25 20:21:08 +0300

    1 завдання зроблено!!!

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const btnStartRef = document.querySelector('button[data-start]');
+const btnStopRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let timerId = null;
+
+btnStartRef.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStopRef.addEventListener('click', () => {
+  clearInterval(timerId);
+});
